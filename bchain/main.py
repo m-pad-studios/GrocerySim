98fc5efcf9de239f58849d8c4fd9aaf82588db87
Mikail-Parkay <56673228+m-pad-studios@users.noreply.github.com>
2021-12-25 18:28:29 -0600

    Update main.py

--- bchain/main.py
+++ bchain/main.py
@@ -1,16 +1,19 @@
-# This is a sample Python script.
+class big_O:
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    values = [1,2,3,4,5,6,7,8,9,10]
+
+    def func_constant(self):
+        print("INSIDE BIG-O CONSTANT")
+        print(self.values)
+
+    def func_linear(self):
+        print("INSIDE BIG-O LINEAR")
+        for v in self.values:
+            print(v)  
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    big = big_O()
+    big.func_constant()
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    big.func_linear()
